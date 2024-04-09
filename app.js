96b637a9fd6704685892c4c17456dbf97332a7e9
x3shat <shatantra@gmail.com>
2024-04-10 00:24:57 +0530

    Added Html ,css and Js file

---
+++ app.js
@@ -0,0 +1,16 @@
+const btn =document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+
+btn.addEventListener('click',()=>{
+    links.classList.toggle('show-links');
+})
+
+
+// toggle list explanation
+// -----------------------
+// if(links.classList.contains('show-links')){
+//     links.classList.remove('show-links');
+// } else {
+//     links.classList.add('show-links');    
+// }
